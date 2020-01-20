0a69b8b87cc0cbf991b4ba78d7c54516363e8deb
sysdevguru <sysdevguru76@gmail.com>
2020-01-20 18:38:05 +0200

    removed timeout from test

--- client/integration_test.go
+++ client/integration_test.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"context"
 	"sync"
 	"testing"
 
@@ -17,7 +18,7 @@ var (
 
 func TestIntegration(t *testing.T) {
 	// create a Basket
-	basket, err := grpcClient.CreateBasket(grpcContext, &pb.CreateBasketRequest{})
+	basket, err := grpcClient.CreateBasket(context.Background(), &pb.CreateBasketRequest{})
 	assert.Nil(t, err)
 	assert.Equal(t, "", basket.Total)
 	basketIDs = append(basketIDs, basket.Id)
@@ -28,7 +29,7 @@ func TestIntegration(t *testing.T) {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			b, err := grpcClient.CreateBasket(grpcContext, &pb.CreateBasketRequest{})
+			b, err := grpcClient.CreateBasket(context.Background(), &pb.CreateBasketRequest{})
 			assert.Equal(t, nil, err)
 			assert.Equal(t, "", b.Total)
 			basketIDs = append(basketIDs, b.Id)
@@ -42,7 +43,7 @@ func TestIntegration(t *testing.T) {
 		wg1.Add(1)
 		go func() {
 			defer wg1.Done()
-			resp, err := grpcClient.AddToBasket(grpcContext, &pb.AddProductRequest{Basketid: basketIDs[0], Product: v})
+			resp, err := grpcClient.AddToBasket(context.Background(), &pb.AddProductRequest{Basketid: basketIDs[0], Product: v})
 			assert.Nil(t, err)
 			assert.Equal(t, 200, int(resp.Code))
 			assert.Equal(t, "", resp.Error)
@@ -51,12 +52,12 @@ func TestIntegration(t *testing.T) {
 	}
 
 	// get first basket amount
-	resp, err := grpcClient.GetBasketAmount(grpcContext, &pb.GetAmountRequest{Basketid: basketIDs[0]})
+	resp, err := grpcClient.GetBasketAmount(context.Background(), &pb.GetAmountRequest{Basketid: basketIDs[0]})
 	assert.Nil(t, err)
 	assert.Equal(t, "70.00€", resp.Total)
 
 	// remove Basket
-	resp1, err := grpcClient.RemoveBasket(grpcContext, &pb.RemoveBasketRequest{Basketid: basketIDs[0]})
+	resp1, err := grpcClient.RemoveBasket(context.Background(), &pb.RemoveBasketRequest{Basketid: basketIDs[0]})
 	assert.Nil(t, err)
 	assert.Equal(t, 200, int(resp1.Code))
 	assert.Equal(t, "", resp1.Error)
